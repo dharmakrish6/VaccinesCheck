1df42fe26f2dd07b9cf733e3ac07c30b4beb6718
dharma <dharma@telio.vn>
2021-05-08 17:37:08 +0530

    push the initial version of script

---
+++ scripts/vaccines.py
@@ -0,0 +1,54 @@
+import requests
+import json
+import time
+import smtplib, ssl
+
+port = 465  # For SSL
+
+context = ssl.create_default_context()
+def mailnow(email,sub):
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login("makemeappz@gmail.com", 'jvzxuiqhbfozbykf')
+        message = 'Subject: {}\n\n{}'.format(sub, 'Please book')
+        server.sendmail('dharma@gmail.com', email, message)
+
+
+while True:
+    pincodes=['560076','560011','560078','560060']
+    for pincode in pincodes:
+        print('Vaccination availability checking for ',pincode)
+        url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin?pincode='+pincode+'&date=08-05-2021'
+        headers = {
+        'User-Agent': 'PostmanRuntime/7.26.8'
+        }
+
+        response = requests.request("GET", url, headers=headers)
+        res = response.json()
+        allcenter=res['centers']
+        for center in allcenter:
+            session_c=center['sessions']
+            print('******************checking******************')
+            session_len=len(session_c)
+            for each in session_c:
+                capacity=int(each['available_capacity'])
+                age=int(each['min_age_limit'])
+
+                if ((age==18) and (capacity> 0)):
+        
+                    message='Vaccines available for 18+ age in '+ pincode + center['name']+' capacity:'+ str(each['available_capacity'])
+                    print(message)
+                    mailnow('suma.s16@gmail.com',message)
+                elif((age==45) and (capacity> 0)):
+                    
+                    message='Vaccines available for 45+ age in'+ pincode + center['name']+' capacity:'+ str(each['available_capacity'])
+                    print(message)
+                    # mailnow('dharmakrish6@gmail.com',message)
+                else:
+                    pass
+    time.sleep(5)
+
+
+                
+            
+    
+    
